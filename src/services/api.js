aa2ede900d559ca798bcfc5a88d2fb825cd08c11
Cassio Tessaro <cassioate@gmail.com>
2020-10-09 21:17:42 -0300

    Correção implementação API BACK

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://https://gerenciamentostefa.herokuapp.com/",
+  baseURL: "https://gerenciamentostefa.herokuapp.com/obra",
 });
 
 export default api;
